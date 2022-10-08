1de24995b4a597b52f09d030a1285f18bb147501
Dmitrij <dmitrij@inbox.lt>
2022-10-08 10:31:08 +0300

    Переделал вторую задачу

--- Program.cs
+++ Program.cs
@@ -14,33 +14,42 @@
 // 32679 -> 6
 
 
-// Console.WriteLine("Введи число: ");
-// int a = Convert.ToInt32(Console.ReadLine());
-// string b = Convert.ToString(a);
-// if (b.Length > 2){
-//   Console.WriteLine("третья цифра -> " + b[2]);
-// }
-// else {
-//   Console.WriteLine("-> третьей цифры нет");
-// }
-
-
+Console.WriteLine("Введи число: ");
+int a = int.Parse(Console.ReadLine()!);
+if (a < 100)
+{
+  Console.WriteLine("-> третьей цифры нет");
+  return;
+}
+int thirdNumber = 0;
+if (a > 99 && a < 1000){
+ thirdNumber = (a % 100) % 10;
+}
+if (a > 999 && a < 10000){
+ thirdNumber = (a % 100) / 10;
+}
+if (a > 9999 && a < 100000){
+ thirdNumber = (a % 1000) / 100;
+}
+Console.WriteLine($"Третья цифра числа {a}->{thirdNumber}");
 
 // Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
 // является ли этот день выходным. Обязательно сделать проверку на ввод чисел меньше 1 и больше 7
 
 
-Console.Write("Введи цифру, обозначающую день недели: ");
-int numberDay = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введи цифру, обозначающую день недели: ");
+// int numberDay = Convert.ToInt32(Console.ReadLine());
 
-void CheckingTheDayOfTheWeek (int numberDay) {
-  if (numberDay == 6 || numberDay == 7) {
-  Console.WriteLine($"{numberDay} -> да");
-  }
-  else if (numberDay < 1 || numberDay > 7) {
-    Console.WriteLine($"{numberDay} -> это вообще не день недели " );
-  }
-  else Console.WriteLine($"{numberDay}  -> нет");
-}
+// void CheckingTheDayOfTheWeek (int numberDay) {
+//   if (numberDay == 6 || numberDay == 7) {
+//   Console.WriteLine($"{numberDay} -> да");
+//   }
+//   else if (numberDay < 1 || numberDay > 7) {
+//     Console.WriteLine($"{numberDay} -> это вообще не день недели " );
+//   }
+//   else Console.WriteLine($"{numberDay}  -> нет");
+// }
 
-CheckingTheDayOfTheWeek(numberDay);
+// CheckingTheDayOfTheWeek(numberDay);
+
+
